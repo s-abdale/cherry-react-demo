2b07c3de59f5a249e77ce8386dbd370da32cb3dd
Safia Abdale <safia.abdale@gmail.com>
2023-02-07 18:33:19 -0500

    Added links to the different pages in the Header component

--- cherry-demo/src/components/shared/Header.js
+++ cherry-demo/src/components/shared/Header.js
@@ -1,9 +1,13 @@
 import './header.css';
+import { Link } from 'react-router-dom';
 
 function Header() {
   return (
     <div className='main-header-body'>
       header component
+      <Link to="/home">Home</Link>
+      <Link to="/about">About</Link>
+      <Link to="/mystery">Mystery</Link>
     </div>
   )
 }
