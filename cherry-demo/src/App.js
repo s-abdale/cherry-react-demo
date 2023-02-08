21ddb2ca8bbe7e9adfc8b9b4d83962a891cc0a23
Safia Abdale <safia.abdale@gmail.com>
2023-02-07 21:52:37 -0500

    Styling: added dark/light mode button

--- cherry-demo/src/App.js
+++ cherry-demo/src/App.js
@@ -1,6 +1,6 @@
 // general imports:
 import './App.css';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 
 // component imports:
@@ -13,11 +13,31 @@ import Footer from './components/shared/Footer';
 
 
 function App() {
-  return (
-    <div className='main-app-body'>
-      <Router>
+  // setup state and useState. set default theme to light
+  const [theme, setTheme] = useState('light');
 
-        <Header/>
+  // function to switch themes. if theme is light then set theme to dark, and vice versa
+  const toggleTheme = () => {
+    theme === 'light'
+      ? setTheme('dark')
+      : setTheme('light');
+  };
+
+  // update the className to whatever the theme is
+  useEffect(() => {
+    document.body.className = theme;
+  }, [theme]);
+
+  return (
+    // the ${theme} class is a variable theme that switches between 'light' and 'dark'
+    <div className={`main-app-body ${theme}`}>
+      
+
+      <Router>
+        <header className="header-container">
+          <Header/>
+          <button onClick={toggleTheme} className="theme-button">{theme === 'light' ? '🌙' : '☀️'}</button>
+        </header>
 
         <Routes>
           <Route path="/home" element={<Home/>}></Route>
