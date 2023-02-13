70cfff449b4fbb947464c794eb059bd0beab98f0
Safia Abdale <safia.abdale@gmail.com>
2023-02-12 20:05:13 -0500

    feat(header): added link to repo for quick access

--- cherry-demo/src/components/shared/Header.js
+++ cherry-demo/src/components/shared/Header.js
@@ -4,7 +4,9 @@ import { Link } from 'react-router-dom';
 function Header() {
   return (
     <div className='main-header-body'>
-      <img src="/cherries_emoji.png" className='header-icon' alt='Cherries emoji'/>
+      <a href='https://github.com/s-abdale/cherry-react-demo'>
+        <img src="/cherries_emoji.png" className='header-icon' alt='Cherries emoji'/>
+      </a>
 
       <div className='grouped-header-links'>
         <Link to="/home" className='header-links'>Home</Link>
